f5c82a68ad9f4f3d8f87d383135ec10c9189ee7c
pixibeestudio <pixibee.studio@gmail.com>
2026-03-20 01:23:03 +0700

    feat: Add core Android dependencies with latest versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,26 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+
+    // Retrofit & Gson
+    implementation("com.squareup.retrofit2:retrofit:3.0.0")
+    implementation("com.squareup.retrofit2:converter-gson:3.0.0")
+
+    // OkHttp Logging Interceptor
+    implementation("com.squareup.okhttp3:logging-interceptor:5.3.2")
+
+    // Glide
+    implementation("com.github.bumptech.glide:glide:5.0.5")
+    annotationProcessor("com.github.bumptech.glide:compiler:5.0.5")
+
+    // AndroidX Lifecycle (ViewModel & LiveData cho Java)
+    implementation("androidx.lifecycle:lifecycle-viewmodel:2.10.0")
+    implementation("androidx.lifecycle:lifecycle-livedata:2.10.0")
+
+    // AndroidX Navigation Component cho Java
+    implementation("androidx.navigation:navigation-fragment:2.9.7")
+    implementation("androidx.navigation:navigation-ui:2.9.7")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
