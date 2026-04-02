4940cbbdf4818b763d8a9619bba6036df29ef557
pixibeestudio <pixibee.studio@gmail.com>
2026-04-02 18:48:53 +0700

    Thêm MPAndroidChart và thiết kế giao diện màn hình Ví/Thu nhập cho Shipper
    - Thêm Jitpack repository vào settings.gradle.kts
    - Thêm MPAndroidChart v3.1.0 vào app/build.gradle.kts
    - Thiết kế layout fragment_shipper_income.xml với:
      * Header cố định 'Ví & Thu nhập'
      * 2 card hiển thị tổng thu nhập và số dư COD
      * Nút nộp tiền mặt (COD)
      * Biểu đồ thu nhập 7 ngày (BarChart)
      * RecyclerView lịch sử giao dịch
    - Tạo item_transaction.xml cho item lịch sử giao dịch
    - Tạo các icon: ic_cash.xml, ic_arrow_up_green.xml, ic_arrow_down_red.xml
    - Build thành công, sẵn sàng để implement logic

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,6 +64,9 @@ dependencies {
     // ViewPager2 cho Banner
     implementation("androidx.viewpager2:viewpager2:1.1.0")
 
+    // MPAndroidChart
+    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
+
     // AndroidX Navigation Component cho Java
     implementation("androidx.navigation:navigation-fragment:2.9.7")
     implementation("androidx.navigation:navigation-ui:2.9.7")
